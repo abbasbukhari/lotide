4b8f434f3964e08f341c7f3ecf00a20fe4970a92
abbas <abbasbukhari2015@gmail.com>
2024-10-19 23:01:47 -0400

    Refactored middle to use modules

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false; 
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false; 
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = function (actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const middle = function (array) {
   const length = array.length;
 
@@ -34,22 +12,4 @@ const middle = function (array) {
   return [array[length / 2 - 1], array[length / 2]];
 };
 
-// TEST CODE
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
-
-const person = { firstName: "Abbas" };
-const firstName = person["firstName"];
-
-console.log(firstName);
-
-const mary = { name: "Mary Sue" };
-mary["name"] = "Mary Jane";
-mary["age"] = 22;
-mary; 
-
-console.log(mary);
+module.exports = middle;
