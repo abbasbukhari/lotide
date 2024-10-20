9820b7b252dc05242f5fac4f9d6575b1818fa8e5
abbas <abbasbukhari2015@gmail.com>
2024-10-19 22:51:46 -0400

    Refactored assertArraysEqual to use modules

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,4 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false; 
-  }
-
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false; 
-    }
-  }
-
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function (array1, array2) {
   const result = eqArrays(array1, array2);
@@ -31,8 +9,5 @@ const assertArraysEqual = function (array1, array2) {
   }
 };
 
-// Test the assertArraysEqual function
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // Should pass
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // Should fail
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // Should pass
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // Should fail
+
+module.exports = assertArraysEqual;
